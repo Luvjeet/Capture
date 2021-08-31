74cc2635dab970a84970b6437153d00103bb8e26
Luvjeet Singh <luzag1402@gmail.com>
2021-08-31 11:46:10 +0530

    About Us created

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// Importing Sections
+import AboutUsSection from '../Components/AboutUsSection';
+
+const AboutUs = () => {
+    return <AboutUsSection />;
+};
+
+export default AboutUs;
