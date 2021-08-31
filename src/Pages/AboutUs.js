ce7743d8a6857298f080043f2c4f8eca69b84b2f
Luvjeet Singh <luzag1402@gmail.com>
2021-08-31 14:01:53 +0530

    Created faq section

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from 'react';
 // Importing Sections
 import AboutUsSection from '../Components/AboutUsSection';
 import ServiceSection from '../Components/ServiceSection';
+import FaqSection from '../Components/FaqSection';
 
 const AboutUs = () => {
     return (
         <>
             <AboutUsSection />
             <ServiceSection />
+            <FaqSection />
         </>
     );
 };
