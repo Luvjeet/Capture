effb680dcfae6c1adfd0ae7277b2aac4958aa63b
Luvjeet Singh <luzag1402@gmail.com>
2021-08-31 18:38:59 +0530

    Used sttyled components for the first time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// Import Global Style File
+import GlobalStyle from './Components/GlobalStyle';
 // Importing pages
 import AboutUs from './Pages/AboutUs';
 
 function App() {
     return (
         <div className="App">
+            <GlobalStyle />
             <AboutUs />
         </div>
     );
