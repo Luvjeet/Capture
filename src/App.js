6707b73e03118379ec05d00b865a9ef8ff418eb9
Luvjeet Singh <luzag1402@gmail.com>
2021-09-13 23:13:37 +0530

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import GlobalStyle from './Components/GlobalStyle';
 // Importing pages
 import AboutUs from './Pages/AboutUs';
+// Navbar
+import Nav from './Components/Nav';
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
+            <Nav />
             <AboutUs />
         </div>
     );
