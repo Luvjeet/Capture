6d1263f5e4ad504d645f02d79bf54267be7b0908
Luvjeet Singh <luzag1402@gmail.com>
2021-09-14 18:49:41 +0530

    Imported React Router

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import GlobalStyle from './Components/GlobalStyle';
 import AboutUs from './Pages/AboutUs';
 // Navbar
 import Nav from './Components/Nav';
+import ContactUs from './Pages/ContactUs';
+import OurWork from './Pages/OurWork';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <AboutUs />
+            <Switch>
+                <Route path="/" exact>
+                    <AboutUs />
+                </Route>
+                <Route path="/work">
+                    <OurWork />
+                </Route>
+                <Route path="/contact">
+                    <ContactUs />
+                </Route>
+            </Switch>
         </div>
     );
 }
